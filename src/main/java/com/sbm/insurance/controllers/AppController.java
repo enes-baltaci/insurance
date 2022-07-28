8fb87cae4b3b8375c60350d725a84e6bfea77823
enes-baltaci <vabregasbaltaci@gmail.com>
2022-07-28 17:29:11 +0300

    Trying to make Submit Form

--- src/main/java/com/sbm/insurance/controllers/AppController.java
+++ src/main/java/com/sbm/insurance/controllers/AppController.java
@@ -1,11 +1,19 @@
 package com.sbm.insurance.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.sbm.insurance.entity.Car;
+import com.sbm.insurance.repository.CarRepository;
 
 @Controller
-public class HelloWorldController {
+public class AppController {
+
+    @Autowired
+    private CarRepository repo;
     
     @GetMapping("")
     public String hello (Model model) {
@@ -16,5 +24,11 @@ public class HelloWorldController {
     @GetMapping("/insurance_page")
     public String insurance() {
         return "insurance_page";
+    }
+
+    @PostMapping("/submit")
+    public String processRegistration (Car car) {
+        repo.save(car);
+        return "index";
     }
 }
