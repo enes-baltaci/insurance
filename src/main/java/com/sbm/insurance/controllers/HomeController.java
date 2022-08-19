7e9e123979b6d228abd6957525b53f5271cf324b
enes-baltaci <vabregasbaltaci@gmail.com>
2022-08-19 12:28:54 +0300

    NavBar added with thymeleaf fragments

--- src/main/java/com/sbm/insurance/controllers/HomeController.java
+++ src/main/java/com/sbm/insurance/controllers/HomeController.java
@@ -1,13 +1,15 @@
 package com.sbm.insurance.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+        model.addAttribute("title", "Insurance App :: Home");
         return "home";
     }
 }
