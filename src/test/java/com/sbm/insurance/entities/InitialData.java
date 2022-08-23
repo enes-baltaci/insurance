524a48b51da6d6055f7c5c2a52871294633b0e74
enes-baltaci <vabregasbaltaci@gmail.com>
2022-08-23 11:22:43 +0300

    InitialData test class for adding initial data into database implemented.

--- src/test/java/com/sbm/insurance/entities/InitialData.java
+++ src/test/java/com/sbm/insurance/entities/InitialData.java
@@ -1,5 +1,6 @@
 package com.sbm.insurance.entities;
 
+import com.sbm.insurance.services.AccountService;
 import com.sbm.insurance.services.CarBrandsService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,13 +9,16 @@ import org.springframework.boot.test.context.SpringBootTest;
 import java.util.ArrayList;
 
 @SpringBootTest
-class CarBrandsTest {
+class InitialData {
 
     @Autowired
     private CarBrandsService carBrandsService;
 
+    @Autowired
+    private AccountService accountService;
+
     @Test
-    void initialBrands() {
+    void initialData() {
 
         ArrayList<String> brands = new ArrayList<String>() {
             {
@@ -32,6 +36,18 @@ class CarBrandsTest {
                 add("Kia");
             }
         };
+
+        ArrayList<Account> accounts = new ArrayList<>() {
+            {
+                add(Account.builder().identity(12345L).name("Enes").surname("Baltacı").email("enes@gmail.com").age(24).city("İstanbul").build());
+                add(Account.builder().identity(12346L).name("Nazif").surname("İlbek").email("nazif@gmail.com").age(30).city("İstanbul").build());
+                add(Account.builder().identity(12347L).name("İrem").surname("Er").email("irem@gmail.com").age(23).city("Muğla").build());
+            }
+        };
+
+        for (Account account : accounts) {
+            accountService.save(account);
+        }
 
         for (String str : brands) {
             CarBrands carBrands = CarBrands.builder().carBrand(str).build();
